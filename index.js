fbe10cdd3bbe322c9bca1a300f018890c404e1ef
Junior Trojilio <osmar.trojilio@gmail.com>
2020-04-08 00:17:58 -0300

    implements chalk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const commander = require('commander')
 const database = require('./database')
 const Heroi = require('./heroi')
+const chalk = require('chalk');
 
 async function main() {
     commander
@@ -13,39 +14,43 @@ async function main() {
         .option('-r --remover', 'remove um herói pelo ID')
         .parse(process.argv)
 
-        const heroi = new Heroi(commander)
+    const heroi = new Heroi(commander)
 
     try {
-        if (!await database.arquivoExists()){
+        if (!await database.arquivoExists()) {
             await database.criarArquivoJson()
         }
 
         if (commander.cadastrar) {
             const resultado = await database.cadastrar(heroi)
-            if (resultado === 0 || resultado > 0){
-            console.log(`🦸‍♂️ Herói cadastrado com sucesso! ID:${ resultado }`)
-            }else{
+            if (resultado === 0 || resultado > 0) {
+                console.log(chalk.green(`🦸‍♂️ Herói cadastrado com sucesso! ID:${resultado}`))
+            } else {
                 console.error('Herói não cadastrado!')
             }
         }
 
-        if(commander.listar){
-           const resultado = await database.listar(heroi.id) 
-           console.log(resultado)
-           return;
+        if (commander.listar) {
+            const resultado = await database.listar(heroi.id)
+            if (!resultado[0]) {
+                console.log(`[Não localizado nenhum herói com o id : ${heroi.id}]`)
+                return;
+            } else {
+                console.log(resultado)
+            }
         }
 
-        if(commander.remover){
-            console.log(heroi)
+        if (commander.remover) {
             const resultado = await database.removerHeroiPorId(heroi.id)
-            if(!resultado){
-                console.error('Não foi possível remover o herói.')
-                return
+            if (!resultado) {
+                console.log(`[Não localizado nenhum herói com o id : ${heroi.id}]`)
+                return;
+            } else {
+                console.log(`O Herói com id: ${heroi.id}, foi removido com sucesso!`)
             }
-            console.log('Herói removido com sucesso!')
         }
     } catch (error) {
-        console.error('deu ruim', error)
+        console.error(chalk.red(`Ops, ${error.message}!`))
     }
 }
 
